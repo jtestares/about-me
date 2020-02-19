240b5bde688ea2ff0b2ec826202fbc985d0f943f
claizelcoubeili <clcepe@up.edu.ph>
2020-02-19 16:18:22 +0800

    added buttons and click listeners

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{addNickname(it)}
     }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        // set the textview to the user input
+        nickNameTextView.text = editText.text
+
+        // hide the input field and done button
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+
+        // show textview for nickname
+        nickNameTextView.visibility = View.VISIBLE
+
+
+        // hide keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
